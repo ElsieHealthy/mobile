9a0b4b0fccb92cab42e66b455211a90bbb758ea9
sanjayadayarathne <sanjayaruwan94@gmail.com>
2023-01-18 14:02:07 +0530

    Calender

--- src/screens/chart/components/Calendar.tsx
+++ src/screens/chart/components/Calendar.tsx
@@ -7,6 +7,8 @@ import {Font} from '../../../Fonts';
 import {CalendarIcon} from '../../../icons/Calendar';
 import {Header} from './Header';
 
+
+
 export const Calendar = () => {
   const getDate = (count: number) => {
     const date = new Date();
